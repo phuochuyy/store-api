8f91a1d8aaa1bff594b69faeeda3ba6bf7c1083d
Phuoc Huy <nph17103@gmail.com>
2025-09-23 20:53:34 +0700

    Fix syntax Rubocop

--- config/initializers/rswag_api.rb
+++ config/initializers/rswag_api.rb
@@ -2,8 +2,8 @@ Rswag::Api.configure do |c|
   # Specify a root folder where Swagger JSON files are generated
   # NOTE: If you're using rswag-specs to generate Swagger, you'll need to ensure
   # that it's configured to generate files in the same folder
-  c.swagger_root = Rails.root.to_s + '/swagger'
-
+  # c.swagger_root = Rails.root.to_s + '/swagger'
+  c.openapi_root = Rails.root.to_s + '/swagger'
   # Inject a lambda function to alter the returned Swagger prior to serialization
   # The function will have access to the rack env for the current request
   # For example, you could leverage this to dynamically assign the "host" property
--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -11,7 +11,8 @@ Rswag::Ui.configure do |c|
   # c.swagger_endpoint '/api-docs/v1/swagger.json', 'API V1 Docs'
   # c.swagger_endpoint '/api-docs/v2/swagger.yaml', 'API V2 Docs'
   # c.swagger_endpoint '/api-docs/v2/swagger.json', 'API V2 Docs'
-  c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'Phone Store API V1'
+  # c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'Phone Store API V1'
+  c.openapi_endpoint '/api-docs/v1/swagger.yaml', 'Phone Store API V1'
 
   # Add Basic Auth in case your API is private
   # c.basic_auth_enabled = true
